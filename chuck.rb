b25ecf074b5ffacd2b1d08fa831a38eb82d93953
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-06-07 10:58:03 -0400

    upadated class

--- chuck.rb
+++ chuck.rb
@@ -6,18 +6,23 @@ class ChuckJokes
   end
 
   def get_joke_by_id(id)
-
+    joke = ChuckNorris::JokeFinder.get_joke
+    id = puts joke.id
   end
 
   def replace_name(name)
-
+    joke = ChuckNorris::JokeFinder.get_joke(first_name: 'Cloud', last_name: 'Strife')
+    puts joke.joke
   end
 
   def random
     joke = ChuckNorris::JokeFinder.get_joke
-    puts joke
+    puts joke.joke
     end
   end
 
-joke = ChuckJokes.new
-puts joke.random
+chuck = ChuckJokes.new
+puts chuck.random
+puts chuck.get_joke_by_id(64)
+puts chuck.help
+puts chuck.replace_name(CLoud)
