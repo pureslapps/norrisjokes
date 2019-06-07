5913aa025f82b5eb4460357136486838e1e0c476
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-06-07 12:39:13 -0400

    got the chuch file working

--- chuck.rb
+++ chuck.rb
@@ -1,28 +1,31 @@
 require "chuck_norris"
 
 class ChuckJokes
+    attr_accessor :id, :name
+    def intialize()
+    end
   def help
 
   end
 
   def get_joke_by_id(id)
-    joke = ChuckNorris::JokeFinder.get_joke
-    id = puts joke.id
+    joke = ChuckNorris::JokeFinder.find_joke(99)
+    puts joke.joke
   end
 
   def replace_name(name)
-    joke = ChuckNorris::JokeFinder.get_joke(first_name: 'Cloud', last_name: 'Strife')
+    joke = ChuckNorris::JokeFinder.get_joke(first_name: 'Alex', last_name: 'Dixon')
     puts joke.joke
   end
 
   def random
     joke = ChuckNorris::JokeFinder.get_joke
     puts joke.joke
-    end
   end
+end
 
 chuck = ChuckJokes.new
-puts chuck.random
-puts chuck.get_joke_by_id(64)
-puts chuck.help
-puts chuck.replace_name(CLoud)
+chuck.help
+chuck.get_joke_by_id(99)
+chuck.replace_name(first_name: 'Alex', last_name: 'Dixon')
+chuck.random
